59b94cd793c792972e566e0e0683f4879a234856
Your Name <you@example.com>
2024-03-17 00:12:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-red-600'>Recipe Calories</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
